3904571f7ed1e50a49b19f6e31474c68ededcd37
Troy Rowan <troy.rowan@outlook.com>
2016-09-21 11:04:45 -0500

    Added comments to separate Allele and Individual Call Rates

--- plink_stats.sh
+++ plink_stats.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#proportion of missing alleles per manifest
 plink --file 58336.160906.100.test_snp50_A --map 9913_SNP50.map --cow --nonfounders --freq --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_A
 
 plink --file 58336.160906.100.test_snp50_B --map 9913_SNP50.map --cow --nonfounders --freq --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_B
@@ -10,7 +11,7 @@ plink --file 227234.160906.100.test_ggpf250_A --map 9913_GGPF250.map --cow --non
 
 plink --file 777962.160906.100.test_hd_A --map 9913_HD.map --cow --nonfounders --freq --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/HD
 
-
+#Gives number of alleles missing for individuals
 
 plink --file 58336.160906.100.test_snp50_A --map 9913_SNP50.map --cow --missing --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Individual_Call_Rates/SNP_50_A_missing  
 
