602773dd2ed98a02a234955e3cb3dfe9befd8f7d
Troy Rowan <troy.rowan@outlook.com>
2016-09-23 12:58:31 -0500

    Changed so that alleles are filtered out before low call rate animals are filtered. The script now reads the .bed file created by the allele call rate filter into the command for the filter for low call rate animals.

--- callrate_filter.sh
+++ callrate_filter.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-#this filters multiple files for callrate on an allele and per animal basis and results in the production of per chip call rate stats. 
-plink --file 58336.160906.100.test_snp50_A --map 9913_SNP50.map --cow --geno 0.05 --mind 0.05 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/allele_individual_filtered/SNP_50_A
+#this filters multiple files first for missing genotypes, and then uses resulting genotypes to filter out individuals with low call rates. 
+plink --file 58336.160906.100.test_snp50_A --map 9913_SNP50.map --cow --geno 0.05 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_A_allele
 
-plink --file 58336.160906.100.test_snp50_B --map 9913_SNP50.map --cow --geno 0.06 --mind 0.06 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/allele_individual_filtered/SNP_50_B
+plink --file 58336.160906.100.test_snp50_B --map 9913_SNP50.map --cow --geno 0.06 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_B_allele
 
-plink --file 58336.160906.100.test_snp50_C --map 9913_SNP50.map --cow --geno 0.06 --mind 0.06 --make-bed  --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/allele_individual_filtered/SNP_50_C
+plink --file 58336.160906.100.test_snp50_C --map 9913_SNP50.map --cow --geno 0.06 --make-bed  --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_C_allele
 
-plink --file 227234.160906.100.test_ggpf250_A --map 9913_GGPF250.map --cow --geno 0.06 --mind 0.15 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/allele_individual_filtered/GGPF250
+plink --file 227234.160906.100.test_ggpf250_A --map 9913_GGPF250.map --cow --geno 0.06 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/GGPF250_allele
 
-plink --file 777962.160906.100.test_hd_A --map 9913_HD.map --cow --geno 0.06 --mind 0.05 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/allele_individual_filtered/HD
+plink --file 777962.160906.100.test_hd_A --map 9913_HD.map --cow --geno 0.06 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/HD_allele
+
+
+
+plink --bfile /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_A_allele --cow --mind 0.05 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Individual_Call_Rates/SNP_50_A_allele_individual
+
+plink --bfile /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_B_allele --cow --mind 0.06 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Individual_Call_Rates/SNP_50_B_allele_individual
+
+plink --bfile /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/SNP_50_C_allele --cow --mind 0.06 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Individual_Call_Rates/SNP_50_C_allele_individual
+
+plink --bfile /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/GGPF250_allele --cow --mind 0.85 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Individual_Call_Rates/GGPF250_allele_individual
+
+plink --bfile /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Allele_Frequencies/HD_allele --cow --mind 0.05 --make-bed --out /CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/Individual_Call_Rates/HD_allele_individual
 
