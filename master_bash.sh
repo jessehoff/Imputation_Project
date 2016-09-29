40faabb2b3362ece0c41897e1f5663611487c818
Troy Rowan <troy.rowan@outlook.com>
2016-09-29 12:50:47 -0500

    This bash script runs, in order, all of the individual bash scripts for our PLINK commands

---
+++ master_bash.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+bash allele_call_rate_stats.sh
+
+bash allele_call_rate_filter.sh
+
+bash individual_call_rate_stats.sh
+
+bash individual_call_rate_filter.sh
+
+bash hwe_stats.sh
+
+bash hwe_filter.sh
