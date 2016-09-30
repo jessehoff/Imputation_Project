f0a64b9ed4e3e09b60e69cebe015050df8f61eed
Troy Rowan <troy.rowan@outlook.com>
2016-09-30 14:20:07 -0500

    more cleanup

--- plink_hd_filter.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash 
-
-
-plink --file 777962.160906.100.test_hd_A --map 9913_HD.map --cow --geno 0.06 --mind 0.05 --make-bed --out HD_cleaned
-plink --file 777962.160906.100.test_SNP50_A --map 9913_HD.map --cow --geno 0.06 --mind 0.05 --make-bed --out HD_cleaned
-
