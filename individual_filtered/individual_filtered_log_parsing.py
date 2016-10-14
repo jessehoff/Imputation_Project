79ca930191055a944441d91cec40f69b89c499b8
Troy Rowan <troy.rowan@outlook.com>
2016-10-14 09:28:51 -0500

    Added timestamp to output and fixed regex issues between assays

--- individual_filtered/individual_filtered_log_parsing.py
+++ individual_filtered/individual_filtered_log_parsing.py
@@ -11,16 +11,17 @@ script, infile = argv
 with open (infile, 'r') as logfile:
     log = logfile.read()
     
-assay = re.findall(r'--bfile ./allele_filtered/([\w_]+)', log)
+assay = re.findall(r'--bfile ./allele_filtered/([\w]+)', log)
+assay1 = re.findall(r'--bfile allele_filtered/([\w]+)', log) 
 mind = re.findall(r'--(mind\s*[\w.]+)', log)
 rem = re.findall(r'([0-9]+) cow removed due to missing genotype data', log)
 rem1 = re.findall(r'([0-9]+) cattle removed due to missing genotype data', log)
 start = re.findall (r'([0-9]+) cattle \(', log)
                 
 
-stats = assay + mind + rem + rem1 + start
+stats = assay + assay1 + mind + rem + rem1 + start
 
-percent = int(stats[2])/int(stats[3])
+percent = float(stats[2])/float(stats[3])
 stats.append(percent)
 
 i = datetime.now()
