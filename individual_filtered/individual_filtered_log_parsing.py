d6999ea6d8a5784cfc965d6b88a0079515facb49
Troy Rowan <troy.rowan@outlook.com>
2016-10-18 12:56:58 -0500

    Added comments

--- individual_filtered/individual_filtered_log_parsing.py
+++ individual_filtered/individual_filtered_log_parsing.py
@@ -1,34 +1,36 @@
 #!/usr/bin/env python 
 
-from datetime import datetime
+
 import sys
 from sys import argv
-script, infile = argv
 import re
 
-script, infile = argv
+# Takes three arguments at commandline (ensure that paths to each is designated)
+script, infile, outfile = argv
 
+# Opens input file from argv
 with open (infile, 'r') as logfile:
     log = logfile.read()
     
-assay = re.findall(r'--bfile ./allele_filtered/([\w]+)', log)
-assay1 = re.findall(r'--bfile allele_filtered/([\w]+)', log) 
-mind = re.findall(r'--(mind\s*[\w.]+)', log)
-rem = re.findall(r'([0-9]+) cow removed due to missing genotype data', log)
-rem1 = re.findall(r'([0-9]+) cattle removed due to missing genotype data', log)
-start = re.findall (r'([0-9]+) cattle \(', log)
+assay = re.findall(r'--bfile ./allele_filtered/([\w]+)', log)#Identifies assay used from input file given to PLINK
+assay1 = re.findall(r'--bfile allele_filtered/([\w]+)', log) #Alternative regex for ID of assay
+mind = re.findall(r'--(mind\s*[\w.]+)', log)# filter used for individuals with low call rates (individuals missing proportion of variants above this value will be removed)
+rem = re.findall(r'([0-9]+) cow removed due to missing genotype data', log)#Extracts number of animals removed (for if single cow is removed)
+rem1 = re.findall(r'([0-9]+) cattle removed due to missing genotype data', log)#Like above, but with multiple animals removed
+start = re.findall (r'([0-9]+) cattle \(', log)#Identifies starting number of animals loaded for proportion calculation
                 
+#List of strings from above regexpressions
+stats = assay + assay1 + mind + rem + rem1 + start 
 
-stats = assay + assay1 + mind + rem + rem1 + start
+#Calculates proportion of animals removed
+proportion = float(stats[2])/float(stats[3])
+stats.append(proportion)
 
-percent = float(stats[2])/float(stats[3])
-stats.append(percent)
+#Searches for and appends to stats list the time when plink filter was run
+time = re.findall(r'Start time: ([0-9 a-z A-Z : .]+)',log)
+stats.append(time)
 
-i = datetime.now()
-timestamp = i.strftime("%m/%d/%Y %H:%M:%S")
-stats.append(timestamp)
-
-
-logfile = open ('individual_filtered_log.csv', 'a')
+#Appends resulting stats list to the output csv file designated by argv 
+logfile = open (outfile, 'a')
 logfile.write(str(stats) + '\n')
 logfile.close()
