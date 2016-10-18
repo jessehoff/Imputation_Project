a7c34a091af6dc6151a0913777b2766c73cded83
Troy Rowan <troy.rowan@outlook.com>
2016-10-18 12:49:22 -0500

    Added comments for each step

--- allele_filtered/allele_filtered_log_parsing.py
+++ allele_filtered/allele_filtered_log_parsing.py
@@ -1,29 +1,33 @@
 #!/usr/bin/env python
 
-from datetime import datetime
+
 import sys
 from sys import argv
 import re
-script, infile = argv
+
+#Takes three arguments on command line to run.  Filepath needed to both input and output files (in different directories).
+#Must be a plink .log file in order for regular expressions to work
+script, infile, outfile = argv
+
 
 with open (infile, 'r') as logfile:
     log = logfile.read()
-    
-assay = re.findall(r'100.test_([\w_]+)', log)
-geno = re.findall(r'--(geno\s*[\w.]+)', log)
-rem = re.findall(r'([0-9]+) variants removed', log)
-start = re.findall(r'([\w]+) variants, ', log)
+
+#Regular expressions look for metadata in log files.      
+assay = re.findall(r'100.test_([\w_]+)', log) #Assay name from input of plink file name
+geno = re.findall(r'--(geno\s*[\w.]+)', log) #The filter used (loci with proportion of genotypes missing > geno will be removed) 
+rem = re.findall(r'([0-9]+) variants removed', log) #This extracts the number of variants removed using the given geno filter
+start = re.findall(r'([\w]+) variants, ', log)#Number of variants loaded from input file (for calculating proportion lost)
 
 
-stats = assay + geno + rem + start
-percent = int(stats[2])/int(stats[3])
-stats.append(percent)
+stats = assay + geno + rem + start #Concatenates strings from previous 4 regular expressions
+proportion = int(stats[2])/int(stats[3])#Calculates a proportion of of variants removed by filter.
+stats.append(proportion)#Appends to list of strings
 
-i = datetime.now()
-timestamp = i.strftime("%m/%d/%Y %H:%M:%S")
-stats.append(timestamp)
+time = re.findall(r'Start time: ([0-9 a-z A-Z : .]+)',ulog) #Regex to grab timestamp from .log file. Appends to list of strings
+stats.append(time)
 
 
-logfile = open ('allele_filtered_log.csv', 'a')
+logfile = open (outfile, 'a') #Appends the list generated in previous steps to designated csv outfile
 logfile.write(str(stats) + '\n')
 logfile.close()
