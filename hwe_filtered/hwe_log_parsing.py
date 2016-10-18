098194951d9ce3137a6a0344b25a8de1b229f23d
Troy Rowan <troy.rowan@outlook.com>
2016-10-18 13:04:29 -0500

    Added comments

--- hwe_filtered/hwe_log_parsing.py
+++ hwe_filtered/hwe_log_parsing.py
@@ -1,26 +1,34 @@
 #!/user/bin/env python
 
-
-from datetime import datetime
 import sys
 from sys import argv
 import re
-script, infile = argv
 
+#Takes three command line arguments (be sure to designate file paths for these as they're likely in different directories)
+script, infile, outfile = argv
+
+#Open input log file from PLINK filter
 with open (infile, 'r') as logfile:
     log = logfile.read()
 
-assay = re.findall(r'individual_filtered/([\w_]+)\n', log)
-hwe = re.findall(r'--(hwe\s*[\w.]+)', log)
-rem = re.findall(r'([0-9]+) variants removed', log)
-start = re.findall(r'([\w]+) variants loaded from ', log)
-time = re.findall(r'Start time: ([0-9 a-z A-Z : .]+)',log)
+#Regular expressions for locating metadata in log file
+assay = re.findall(r'individual_filtered/([\w_]+)\n', log) #Locates assay ID
+hwe = re.findall(r'--(hwe\s*[\w.]+)', log)#Value of hwe filter
+rem = re.findall(r'([0-9]+) variants removed', log)#Number of variants removed by filter
+start = re.findall(r'([\w]+) variants loaded from ', log)#Number of variants loaded before filtering
+
+#Concatenates regular expressions above into "stats" list 
 stats = assay + hwe + rem + start
+
+#Calculates proportion of variants removed compared to total then appends to "stats"
 percent = int(stats[2])/int(stats[3])
 stats.append(percent)
+
+#Locates time when filter was run by PLINK then appends this to stats list
 time = re.findall(r'Start time: ([0-9 a-z A-Z : .]+)',log)
 stats = stats +time
 
-logfile = open ('hwe_filter_logging.csv', 'a')
+#Appends stats list to the end of specified csv file
+logfile = open (outfile, 'a')
 logfile.write(str(stats) + '\n')
 logfile.close()
