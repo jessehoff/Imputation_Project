2d830d6d839c47a475135e7a4aafa0ad0230bcbe
Troy Rowan <troy.rowan@outlook.com>
2016-10-24 22:04:07 -0500

    Added filter step name to output

--- allele_filtered/allele_filtered_log_parsing.py
+++ allele_filtered/allele_filtered_log_parsing.py
@@ -13,6 +13,7 @@ script, infile, outfile = argv
 with open (infile, 'r') as logfile:
     log = logfile.read()
 
+action = ['Allele Filtering Step']
 #Regular expressions look for metadata in log files.      
 assay = re.findall(r'100.test_([\w_]+)', log) #Assay name from input of plink file name
 geno = re.findall(r'--(geno\s*[\w.]+)', log) #The filter used (loci with proportion of genotypes missing > geno will be removed) 
@@ -20,14 +21,14 @@ rem = re.findall(r'([0-9]+) variants removed', log) #This extracts the number of
 start = re.findall(r'([\w]+) variants, ', log)#Number of variants loaded from input file (for calculating proportion lost)
 
 
-stats = assay + geno + rem + start #Concatenates strings from previous 4 regular expressions
-proportion = int(stats[2])/int(stats[3])#Calculates a proportion of of variants removed by filter.
+stats = action + assay + geno + rem + start #Concatenates strings from previous 4 regular expressions
+proportion = int(stats[3])/int(stats[4])#Calculates a proportion of of variants removed by filter.
 stats.append(proportion)#Appends to list of strings
 
-time = re.findall(r'Start time: ([0-9 a-z A-Z : .]+)',ulog) #Regex to grab timestamp from .log file. Appends to list of strings
+time = re.findall(r'Start time: ([0-9 a-z A-Z : .]+)', log) #Regex to grab timestamp from .log file. Appends to list of strings
 stats.append(time)
 
 
 logfile = open (outfile, 'a') #Appends the list generated in previous steps to designated csv outfile
-logfile.write(str(stats) + '\n')
+logfile.write( str(stats) + '\n')
 logfile.close()
