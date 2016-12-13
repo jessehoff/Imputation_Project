96ef25e6f65720460e50644e3f96cce68fd9bd64
Jesse Hoff <jesse.hoff@gmail.com>
2016-12-13 09:52:16 -0600

    snakefile uses sample names and the hwe file list maker is bed only glob based. started figuring out how to make test animal set

--- hwe_filtered/file_list_maker.py
+++ hwe_filtered/file_list_maker.py
@@ -1,19 +1,20 @@
 import glob
 import pandas as pd
 import numpy as np
+import sys
+import os
+output=sys.argv[1]
+
+curd = os.getcwd() 
 
 bed = []	#Set up empty lists for each needed file type     
 bim = []
 fam = []
 
-for i in glob.glob('/CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/hwe_filtered/*.bed'): #Each of these searches for any file in directory that ends in "bed", "bim", or "fam"
-    bed.append(i)
-for j in glob.glob('/CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/hwe_filtered/*.bim'):
-    bim.append(j)
-for k in glob.glob('/CIFS/MUG01_N/taylorjerr/JLH/160906_imputation_test/hwe_filtered/*.fam'):
-    fam.append(k)
-    
-with open('allfiles.txt', 'w+'):	#Open allfiles.txt that will be used in merge step
-    data = np.array([bed, bim, fam])	# Reads three lists into a numpy array, then transposes so that assays are rows, and file types are columns
-    data = data.T
-np.savetxt('allfiles.txt', data, fmt=['%s', '%s','%s']) 	#Writes to text file
+for i in glob.glob(curd  + '/hwe_filtered/*1611*.bed'): #Each of these searches for any file in directory that ends in "bed", "bim", or "fam"
+    bed.append(i.rstrip('.bed'))
+
+with open(output, 'w+') as f:	#Open allfiles.txt that will be used in merge step
+   for t in bed:
+       f.write(t + '\n')
+       
