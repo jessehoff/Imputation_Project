fe2979f8ae696be623708a8f738ad7da80bed361
Troy Rowan <troy.rowan@outlook.com>
2016-12-13 15:00:24 -0600

    prints FID and IID to txt file so that PLINK remove can work

--- sex_determination/missexed_animals_filter.py
+++ sex_determination/missexed_animals_filter.py
@@ -16,4 +16,4 @@ my_list =problems['IID'].tolist()
 
 with open('missexed_animals.txt','w') as file:		#Writes to missexed_animals.txt -- This is write only, not append, so it will refresh this every time that the step is run
     for item in my_list:
-        file.write(item +"\n")
+        file.write(item + ' '+item +"\n")
