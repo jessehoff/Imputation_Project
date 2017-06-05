d3cf8abd278f2e06fccd10afdef5682afef610c1
Troy Rowan <troy.rowan@outlook.com>
2017-06-05 11:05:31 -0500

    modified to run much quicker and output base-by-base correlations.  Functional in Python 2

--- bin/vcf_impacc.py
+++ bin/vcf_impacc.py
@@ -6,42 +6,36 @@ import seaborn as sb
 #%matplotlib inline
 import glob as glob
 from sys import argv
+from itertools import islice
 
-script, true, imputed, log = argv
+script, true, imputed, log, out = argv
 
 truedict = {}
-trueids = []
+genskey = {'./.':'NA', '0/0':'0', '0/1':'1' ,'1/1':'2'}
 with open(true, "r") as t:
-    for line in t:
-        if not line.startswith("#"):
-            cp = (line.split("\t")[0], line.split("\t")[1])       #
-            pos = ":".join(cp)
-            gen = line.split("\t")[9:]
-            genotypes = [(x.split("/")) for x in gen]
-            intgt = [[int(y) for y in x if y != '.' and y != '.\n']for x in genotypes]
-            alt = [sum(x) for x in intgt]
-            truedict[pos] = alt
-        if line.startswith("#"):
-            trueids.append(line)
-samples = trueids[-1].split("\t")[9:]
+	head = list(islice(t,6))
+	samples = t.next().strip().split()[9:]
+	for line in t:
+		splat = line.strip().split()
+		cp = (splat[0], splat[1])
+		pos = ":".join(cp)
+		gen = splat[9:]
+		alt = [genskey[i] for i in gen]
+		truedict[pos] = alt
 truedf = pd.DataFrame.from_dict(truedict, orient='index')
 truedf.columns = samples
 
 impdict = {}
-impids = []
 with open(imputed, "r") as i:
-    for line in i:
-        if not line.startswith("#"):
-            cp = (line.split("\t")[0], line.split("\t")[1])       #
-            pos = ":".join(cp)
-            gen = line.split("\t")[9:]
-            genotypes = [(x.split("/")) for x in gen]
-            intgt = [[int(y) for y in x if y != '.' and y != '.\n']for x in genotypes]
-            alt = [sum(x) for x in intgt]
-            impdict[pos] = alt
-        if line.startswith("#"):
-            impids.append(line)
-impsamples = impids[-1].split("\t")[9:]
+	head = list(islice(i,6))
+	impsamples = i.next().strip().split()[9:]
+	for line in i:
+		splat = line.strip().split()
+		cp = (splat[0], splat[1])
+		pos = ":".join(cp)
+		gen = splat[9:]
+		alt = [genskey[p] for p in gen]
+		impdict[pos] = alt
 impdf = pd.DataFrame.from_dict(impdict, orient='index')
 impdf.columns = impsamples
 
@@ -49,9 +43,13 @@ corrs=impdf.corrwith(truedf, axis = 1)
 cor = pd.DataFrame(corrs)
 cor = cor.rename(index=str, columns={0:"correlation"})
 
+cor.to_csv(out)
+
 outfile = "./test_correlations.txt"
 assay = imputed.split("/")[1].strip(".vcf")
 correlation = cor.mean()[0]
 
 with open (log, 'a') as out:
-    out.write(assay + "\t" + str(correlation)+"\n")
+	out.write(assay + "\t" + str(correlation)+"\n")
+
+accuracy_test/merged_refs/F250_HD_merged.chr25.vcf
